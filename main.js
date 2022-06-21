335c96dd0826300f68725a4745cb33789361c5ec
Minh Tran Cong <minh.trancong@VP1-SE-CL175-01>
2022-06-21 10:59:52 +0900

    done option 2

--- main.js
+++ main.js
@@ -17,25 +17,67 @@ const hasGetUserMedia = () => {
 
 const handleVideoByUserMedia = () => {
     let width = 320;
-    var height = 0;
+    let height = 0;
 
-    var streaming = false;
+    let streaming = false;
 
-    var video = document.getElementById('video');
-    var canvas = document.getElementById('canvas');
-    var photo = document.getElementById('photo');
-    var startbutton = document.getElementById('startbutton');
+    const video = document.getElementById('video');
+    const canvas = document.getElementById('canvas');
+    const photo = document.getElementById('photo');
+    const startVideoBtn = document.getElementById('start-video-btn');
+    const captureBtn = document.getElementById('capture-btn');
+
+    captureBtn.disabled = true;
     
-    if (hasGetUserMedia()) {
-        navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(stream => {
-            video.srcObject = stream;
-            video.play();
-        }).catch(err => {
-            console.log("Errors !! : " + err);
-        })
-    } else {
-        alert("Browser not support User Media")
+    startVideoBtn.onclick = (event) => {
+        if (hasGetUserMedia()) {
+            navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(stream => {
+                video.srcObject = stream;
+                video.play();
+
+                video.oncanplay = (event) =>{
+                    if(!streaming){
+                        height = video.videoHeight / (video.videoWidth/width);
+                        video.setAttribute('width', width);
+                        video.setAttribute('height', height);
+                        canvas.setAttribute('width', width);
+                        canvas.setAttribute('height', height);
+                        captureBtn.disabled = false;
+                        streaming = true;
+                    }
+                }
+            }).catch(err => {
+                console.log("Errors !! : " + err);
+            })
+        } else {
+            alert("Browser not support User Media")
+        }
     }
+
+    captureBtn.onclick = (event) => {
+        let context = canvas.getContext('2d');
+        if (width && height) {
+            canvas.width = width;
+            canvas.height = height;
+            context.drawImage(video, 0, 0, width, height);
+    
+            let data = canvas.toDataURL('image/png');
+            photo.setAttribute('src', data);
+        }else{
+            context.fillStyle = "#AAA";
+            context.fillRect(0, 0, canvas.width, canvas.height);
+
+            let data = canvas.toDataURL('image/png');
+            photo.setAttribute('src', data);
+        }
+    }
+}
+
+const takePicture = (video) => {
+    const canvas = document.getElementById('canvas');
+
+    let context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, )
 }
 
 document.addEventListener('DOMContentLoaded', () => {
