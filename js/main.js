0db7738bff26243a709966f53d60a3fe1432a688
unknown <minh.trancong@NSK.LOCAL>
2022-06-23 11:01:25 +0900

    complete preview img function

--- js/main.js
+++ js/main.js
@@ -8,33 +8,75 @@ const video = document.getElementById('video');
 const canvas = document.getElementById('temp-canvas');
 const startVideoBtn = document.getElementById('start-video-btn');
 const captureBtn = document.getElementById('capture-btn');
+
+const imageUpBtn = document.querySelector(".preview-image__possition-up");
+const imageDownBtn = document.querySelector(".preview-image__possition-down");
+const imageleftBtn = document.querySelector(".preview-image__possition-left");
+const imageRightBtn = document.querySelector(".preview-image__possition-right");
+const imageZoomInBtn = document.querySelector(".preview-image__zoom-in");
+const imageZoomOutBtn = document.querySelector(".preview-image__zoom-out");
+
+const savePreviewImgBtn = document.querySelector(".preview-image__save");
+const downloadImgBtn = document.querySelector(".preview-image__download");
+const resultImg = document.querySelector(".image-result");
+
 const previewModal = new bootstrap.Modal(document.getElementById("preview-modal"), {});
 
-var cropper = new Cropper(previewImage, {
-    aspectRatio: 16 / 9,
+
+var cropper = null;
+var option = {
+    aspectRatio: 1/1,
     dragMode: 'move',
+    preview: '.img-preview',
     viewMode: 2,
-    autoCropArea: 1,
+    modal: false,
     background: false,
-  });
+    ready: function(){
+        imageZoomInBtn.onclick = () => cropper.zoom(0.1);
+        imageZoomOutBtn.onclick = () => cropper.zoom(-0.1);
+
+        imageUpBtn.onclick = () => {
+            currCropData = cropper.getCropBoxData();
+            if(currCropData.top > 0) cropper.setCropBoxData({...currCropData, top: currCropData.top - 10})
+        }
+
+        imageDownBtn.onclick = () => {
+            currCropData = cropper.getCropBoxData();
+            if(currCropData.top > 0) cropper.setCropBoxData({...currCropData, top: currCropData.top + 10})
+        }
+
+        imageleftBtn.onclick = () => {
+            currCropData = cropper.getCropBoxData();
+            if(currCropData.left > 0) cropper.setCropBoxData({...currCropData, top: currCropData.left - 10})
+        }
+
+        imageRightBtn.onclick = () => {
+            currCropData = cropper.getCropBoxData();
+            if(currCropData.right > 0) cropper.setCropBoxData({...currCropData, top: currCropData.left + 10})
+        }
+
+        savePreviewImgBtn.onclick = () => {
+            cropper.getCroppedCanvas().toBlob(blob => {
+                let croppedImgUrl = URL.createObjectURL(blob);
+                resultImg.src = croppedImgUrl;
+                previewModal.hide();
+            })
+        }
+    }
+}
 
 const handleSubmitImageByMediaCapture = (elm) => {
     elm.onchange = event => {
         const [file] = htmlMediaCapture.files;
- 
+
         if (file) {
-          previewModal.show();
-          setTimeout(()=>{
-              previewImage.src = URL.createObjectURL(file);
-              cropper.destroy();
-              
-              cropper = new Cropper(previewImage, {
-                aspectRatio: 16 / 9,
-                dragMode: 'move',
-                viewMode: 2,
-                background: false,
-              });
-            }, 100)
+            previewModal.show();
+            setTimeout(() => {
+                previewImage.src = URL.createObjectURL(file);
+                if (cropper) cropper.destroy();
+
+                cropper = new Cropper(previewImage, option);
+            }, 150)
         }
 
     }
@@ -49,29 +91,29 @@ const handleVideoByUserMedia = () => {
     let streaming = false;
 
     captureBtn.disabled = true;
-    
+
     startVideoBtn.onclick = (event) => {
         // if(hasGetUserMedia()){
-            navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(stream => {
-                video.srcObject = stream;
-                video.play();
+        navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(stream => {
+            video.srcObject = stream;
+            video.play();
 
-                video.oncanplay = (event) =>{
+            video.oncanplay = (event) => {
+                captureBtn.disabled = false;
+                if (!streaming) {
+
+                    video.width = video.videoWidth;
+                    video.height = video.videoHeight;
                     captureBtn.disabled = false;
-                    if(!streaming){
-                       
-                        video.width = video.videoWidth;
-                        video.height = video.videoHeight;
-                        captureBtn.disabled = false;
-                        streaming = true;
-                    }
+                    streaming = true;
                 }
-            }).catch(err => {
-                alert("Errors !! : " + err);
-            })
+            }
+        }).catch(err => {
+            alert("Errors !! : " + err);
+        })
         // }
         // else{
-            // alert("Your browser dose not supported this function !");
+        // alert("Your browser dose not supported this function !");
         // }
     }
 
@@ -81,24 +123,46 @@ const handleVideoByUserMedia = () => {
 
         canvas.width = video.width;
         canvas.height = video.height;
-        
+
         context.drawImage(video, 0, 0, video.width, video.height);
         previewImage.src = canvas.toDataURL();
         previewModal.show();
-        
+
         cropper.destroy();
-            
+
         cropper = new Cropper(previewImage, {
-          aspectRatio: 16 / 9,
-          dragMode: 'move',
-          viewMode: 2,
-          background: false,
+            aspectRatio: 16 / 9,
+            dragMode: 'move',
+            viewMode: 2,
+            background: false,
         });
-        
+
+    }
+}
+
+const handleImageChange = () => {
+    downloadImgBtn.disabled = true;
+    
+    resultImg.onload = () => {
+        downloadImgBtn.disabled = false;
+    }
+}
+
+const handleImageDownload = () => {
+    let aTag = document.createElement('a');
+
+    handleImageChange();
+    downloadImgBtn.onclick = () => {
+        let currDate = new Date();
+
+        aTag.href = resultImg.src;
+        aTag.download = `download_${currDate.toLocaleString()}.jpg`;
+        aTag.click();
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     handleSubmitImageByMediaCapture(htmlMediaCapture);
     handleVideoByUserMedia();
+    handleImageDownload();
 })
