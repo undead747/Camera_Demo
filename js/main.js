b1507bf767fa0728d6656264568914a3eabd8172
unknown <minh.trancong@NSK.LOCAL>
2022-07-01 15:10:34 +0900

    test

--- js/main.js
+++ js/main.js
@@ -185,8 +185,8 @@ const handleSubmitImageByMediaCapture = (elm) => {
             
             setTimeout(async () => {
                 let inputImgURL = URL.createObjectURL(file);
-                // let drawnImgSrc = await drawImageInMiddleCanvas(inputImgURL);
-                await croppieInit(inputImgURL);
+                let drawnImgSrc = await drawImageInMiddleCanvas(inputImgURL);
+                await croppieInit(drawnImgSrc);
                 loadingAnimation().end();
             }, 170)
 
